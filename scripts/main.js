b6ed4c99ed88594ba02ce8099ad2468251a5b9e9
Sean R Reid <sean@torchcodelab.com>
2020-02-11 14:55:11 -0500

    Playing around with options

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 // User specific scripts go here
+
+$('#launchModal').click(function(e) {
+  $('#exampleModal').modal({
+    keyboard: false
+  });
+});
+
+$('#doFade').click(function(e) {
+  $('#fadeIn').fadeIn('slow');
+});
